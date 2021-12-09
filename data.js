149315f53a7c448c33068cf4d406d3e8805b3b1d
kawthar <kawtharzeayter.6@gmail.com>
2021-12-09 12:00:58 +0200

    created slider

---
+++ data.js
@@ -0,0 +1,37 @@
+export default [
+    {
+      type: 'Online_Tutors',
+      imageUri: require('./assets/online.png'),
+      heading: 'Vibrant colors',
+      description: 'Four on-trend colorways to seamlessly suit your style.',
+      key: 'first',
+      color: '#9dcdfa',
+    },
+    {
+      type: 'Login admin',
+      imageUri: require('./assets/Admin.png'),
+      heading: 'Redefined sound',
+      description: 'A bold statement tuned to perfection.',
+      key: 'second',
+      color: '#db9efa',
+    },
+    {
+      type: 'Login Parent',
+      imageUri: require('./assets/Parent.png'),
+      heading: 'Great quality',
+      description:
+        'An Urbanears classic! Listen-all-day fit. Striking the perfect balance of effortless technology',
+      key: 'third',
+      color: '#999',
+    },
+    {
+      type: 'Login Teacher',
+      imageUri: require('./assets/teacher.png'),
+      heading: 'From Sweden',
+      description:
+        'The “Plattan” in Plattan headphones is Swedish for “the slab.”',
+      key: 'fourth',
+      color: '#a1e3a1',
+    },
+  ];
+  
